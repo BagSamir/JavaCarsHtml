76f31ec7faba1e291411cfac9af10c049021b1ea
Samir Bagiev <bagsamitoo@gmail.com>
2024-07-20 18:09:04 +0300

    create cars html

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import web.model.Cars;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService {
+    private List<Cars> cars = new ArrayList<>();
+
+    public List<Cars> getAllCars(int count) {
+        cars.add(new Cars("BMW", "Black", 200));
+        cars.add(new Cars("Audi", "White", 100));
+        cars.add(new Cars("Lada", "Silver", 300));
+        cars.add(new Cars("Mercedes", "Red", 200));
+        cars.add(new Cars("Volvo", "Blue", 1000));
+
+        if (count == 0 || count >= 5) return cars;
+
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+
+
+}
